545731b2f5313b3e0f184e8096e1cc42b48f05b1
milovan-minic <milovan.minic@gmail.com>
2015-12-31 11:26:15 +0100

    Updated importer script

--- otherEmergencyContactsImporter.php
+++ otherEmergencyContactsImporter.php
@@ -1,31 +1,70 @@
-<?php require_once(DIR . '/../ToolHelper.php'); require_once(DIR . '/PhoneNumberFilter.php');
+<?php
+require_once(__DIR__ . '/../ToolHelper.php');
+require_once(__DIR__ . '/PhoneNumberFilter.php');
 
 class OtherEmergencyContactsImporter extends Model_Import_Base {
 
     /**
+     * @param $input
+     * @return array
+     * Function takes name or names and splits them into
+     * one or two names based on so far identified delimiters
+     * adds surname of second person to first
+     * and returns array with both person formed name
+     */
+    protected function filterNames($input)
+    {
+        $validNames = array();
 
-    @param $input
-    @return array
-    Function takes name or names and splits them into
-    one or two names based on so far identified delimiters
-    adds surname of second person to first
-    and returns array with both person formed name */ protected function filterNames($input) { $validNames = array();
+        // Define all delimiters for names
+        $namesSeparator = array(
+            '&' => '',
+            ' AND ' => '',
+            '/' => '',
+            '\\' => '',
+        );
 
-// Define all delimiters for names $namesSeparator = array( '&' => '', ' AND ' => '', '/' => '', '\' => '', );
+        // Replace all white spaces
+        $input = trim(preg_replace('/\s+/', ' ', $input));
+        $input = preg_replace('/\s+/', ' ', strtoupper($input));
+        $names = array($input);
+        foreach ($namesSeparator as $delimiter => $replace) {
+            if (false !== strpos($input, $delimiter)) {
+                $names = explode($delimiter, $input);
+                break;
+            }
+        }
 
-// Replace all white spaces $input = trim(preg_replace('/\s+/', ' ', $input)); $input = preg_replace('/\s+/', ' ', strtoupper($input)); $names = array($input); foreach ($namesSeparator as $delimiter => $replace) { if (false !== strpos($input, $delimiter)) { $names = explode($delimiter, $input); break; } }
+        $names = array_unique($names);
+        // Trims all white characters within name
+        array_walk($names, function (&$val) {
+            $val = trim($val);
+        });
 
-    $names = array_unique($names); // Trims all white characters within name array_walk($names, function (&$val) { $val = trim($val); });
+        $namesCount = count($names);
+        // Assigns Name to name1 if there is no delimiter from the pattern array
+        if ($namesCount == 1) {
+            $validNames[] = $names[0];
+        }
+        // If there is more than one name, takes second name and assigns value to name2 and adds surname to name1
+        if ($namesCount == 2) {
+            $name2 = $names[1];
+            $names2 = explode(" ", $name2);
+            array_shift($names2);
+            $validNames[] = $names[0] . ' ' . implode(' ', $names2);
+            $validNames[] = $name2;
+        }
 
-    $namesCount = count($names); // Assigns Name to name1 if there is no delimiter from the pattern array if ($namesCount == 1) { $validNames[] = $names[0]; } // If there is more than one name, takes second name and assigns value to name2 and adds surname to name1 if ($namesCount == 2) { $name2 = $names[1]; $names2 = explode(" ", $name2); array_shift($names2); $validNames[] = $names[0] . ' ' . implode(' ', $names2); $validNames[] = $name2; }
+        return $validNames;
+    }
 
-    return $validNames; }
 
     public function processFile($fileName, $startRow) {
 
         $staffXml = simplexml_load_file($fileName);
 
         $columnNames = [];
+
 
         $c = 0;
         foreach ($staffXml->METADATA->FIELD as $fieldXml) {
@@ -44,136 +83,155 @@ class OtherEmergencyContactsImporter extends Model_Import_Base {
             $rows[] = $rowData;
         }
 
+
         foreach ($rows as $rowNumber => $row) {
             if ($rowNumber >= $startRow) {
                 echo "Processing Row: $rowNumber\n";
-// if($rowNumber >= 400) { $this->processRow($rowNumber, $row, 4); // } } else echo "Skipping row :" . $rowNumber . " because it is less than startRow: $startRow\n"; } echo "Completed processing of ".($rowNumber+1)." rows\n";
+//                if($rowNumber >= 400) {
+                $this->processRow($rowNumber, $row, 4);
+//                }
+            } else echo "Skipping row :" . $rowNumber . " because it is less than startRow: $startRow\n";
+        }
+        echo "Completed processing of ".($rowNumber+1)." rows\n";
 
+
+    }
+
+    public function processRow($rowNumber, $row, $emergencyContactPriority) {
+
+//        $input = 'Margaret Cleator Grandma 817791/479170w/978444/B Harvey 628287/481674';
+        if($row['Contacts (other)']) {
+            $input = $row['Contacts (other)'];
+            preg_match_all('/(\D{3,})([\d\s\/who]+)/si', $input, $matches);
+
+            $nameString = null;
+            $numberString = null;
+
+            $names = null;
+            $telephoneNumbers = null;
+
+            foreach ($matches[0] as $index => $match) {
+                if (isset($matches[1][$index]) && isset($matches[2][$index])) {
+                    $nameString = $matches[1][$index]; // Names
+                    $numberString = $matches[2][$index]; // Telephone numbers
+                }
+
+                // Names
+                $names = $this->filterNames($nameString);
+                // Numbers
+//                $telephoneNumbers = $this->filterNumber($numberString);
             }
 
-            public function processRow($rowNumber, $row, $emergencyContactPriority) { // $input = 'Margaret Cleator Grandma 817791/479170w/978444/B Harvey 628287/481674'; if($row['Contacts (other)']) { $input = $row['Contacts (other)']; preg_match_all('/(\D{3,})([\d\s\/who]+)/si', $input, $matches);
+            if($names){
 
-                $nameString = null;
-                $numberString = null;
+                foreach($names as $name) {
+                    if($name) {
+                        // Create person for each name
+                        $guardianPerson = new \Misa\Model\Person();
 
-                $names = null;
-                $telephoneNumbers = null;
+                        $names = explode(" ", $name);
+                        $titles = array(
+                            'Ms',
+                            'Mr',
+                            'Miss'
+                        );
 
-                foreach ($matches[0] as $index => $match) {
-                    if (isset($matches[1][$index]) && isset($matches[2][$index])) {
-                        $nameString = $matches[1][$index]; // Names
-                        $numberString = $matches[2][$index]; // Telephone numbers
-                    }
+                        foreach($titles as $key => $value) {
+                            if(strpos(strtolower($names[0]), strtolower($value)) !== false) {
+                                unset($names[0]);
+                                break;
+                            }
+                        }
 
-                    // Names
-                    $names = $this->filterNames($nameString);
-                    // Numbers
-// $telephoneNumbers = $this->filterNumber($numberString); }
+                        $firstName = array_shift($names);
+                        $lastName = implode(" ", $names);
 
-                    if($names){
+                        if(strlen(trim($firstName)) > 0) {
+                            $guardianPerson->setLegalFirstName(trim($firstName));
+                        }
+                        if(strlen(trim($lastName)) > 0) {
+                            $guardianPerson->setLegalLastName(trim($lastName));
+                        }
 
-                        foreach($names as $name) {
-                            if($name) {
-                                // Create person for each name
-                                $guardianPerson = new \Misa\Model\Person();
+//                        $guardianPerson->setLegalFirstName($name);
 
-                                $names = explode(" ", $name);
-                                $titles = array(
-                                    'Ms',
-                                    'Mr',
-                                    'Miss'
-                                );
+                        if(strlen(trim($name)) >= 1) {
+                            $guardianPerson->tag("class", __CLASS__);
+                            $guardianPerson->tag("guardian_name", $name);
 
-                                foreach($titles as $key => $value) {
-                                    if(strpos(strtolower($names[0]), strtolower($value)) !== false) {
-                                        unset($names[0]);
-                                        break;
-                                    }
-                                }
+                            $this->synchronizeUsingUserTags($guardianPerson);
+                        }
 
-                                $firstName = array_shift($names);
-                                $lastName = implode(" ", $names);
+                        // Create Guardian for every name
+                        $guardian = new \Misa\Model\Guardian();
+                        $guardian->setPerson($guardianPerson);
 
-                                if(strlen(trim($firstName)) > 0) {
-                                    $guardianPerson->setLegalFirstName(trim($firstName));
-                                }
-                                if(strlen(trim($lastName)) > 0) {
-                                    $guardianPerson->setLegalLastName(trim($lastName));
-                                }
-// $guardianPerson->setLegalFirstName($name);
+                        if(strlen(trim($name)) >= 1) {
+                            $guardian->tag("class", __CLASS__);
+                            $guardian->tag("guardian", $name);
 
-                                if(strlen(trim($name)) >= 1) {
-                                    $guardianPerson->tag("class", __CLASS__);
-                                    $guardianPerson->tag("guardian_name", $name);
+                            $this->synchronizeUsingUserTags($guardian);
+                        }
 
-                                    $this->synchronizeUsingUserTags($guardianPerson);
-                                }
+                        // Create student guardian relationship
+                        $pupilUniqueNumber = str_replace('-', '', $row["Unique number"]);
+                        $student = $this->getModelBySimpleQuery(
+                            \Misa\Resource\ResourceType::STUDENT,
+                            array(),
+                            array("unique_number" => $pupilUniqueNumber)
+                        );
 
-                                // Create Guardian for every name
-                                $guardian = new \Misa\Model\Guardian();
-                                $guardian->setPerson($guardianPerson);
+                        $studentGuardianRelationship = new \Misa\Model\StudentGuardianRelationship();
+                        $studentGuardianRelationship->setStudent($student);
+                        $studentGuardianRelationship->setGuardian($guardian);
+                        $studentGuardianRelationship->setEmergencyContactPriority($emergencyContactPriority);
 
-                                if(strlen(trim($name)) >= 1) {
-                                    $guardian->tag("class", __CLASS__);
-                                    $guardian->tag("guardian", $name);
+                        $relationship = $this->getModelBySimpleQuery(
+                            \Misa\Resource\ResourceType::GUARDIAN_RELATIONSHIP_TYPE,
+                            array("code" => "OTHR"),
+                            array()
+                        );
 
-                                    $this->synchronizeUsingUserTags($guardian);
-                                }
+                        $studentGuardianRelationship->setGuardianRelationshipType($relationship);
 
-                                // Create student guardian relationship
-                                $pupilUniqueNumber = str_replace('-', '', $row["Unique number"]);
-                                $student = $this->getModelBySimpleQuery(
-                                    \Misa\Resource\ResourceType::STUDENT,
-                                    array(),
-                                    array("unique_number" => $pupilUniqueNumber)
-                                );
+                        if(strlen(trim($name)) >= 1) {
+                            $studentGuardianRelationship->tag("class", __CLASS__);
+                            $studentGuardianRelationship->tag("pupil_unique_number", $pupilUniqueNumber);
+                            $studentGuardianRelationship->tag("guardian_name", $name);
 
-                                $studentGuardianRelationship = new \Misa\Model\StudentGuardianRelationship();
-                                $studentGuardianRelationship->setStudent($student);
-                                $studentGuardianRelationship->setGuardian($guardian);
-                                $studentGuardianRelationship->setEmergencyContactPriority($emergencyContactPriority);
+                            $this->synchronizeUsingUserTags($studentGuardianRelationship);
+                        }
 
-                                $relationship = $this->getModelBySimpleQuery(
-                                    \Misa\Resource\ResourceType::GUARDIAN_RELATIONSHIP_TYPE,
-                                    array("code" => "OTHR"),
-                                    array()
-                                );
+                        // Create telephone number for every person
+                        /** @var \Misa\Model\TelephoneNumber[] $phoneNumbers */
+                        $phoneNumbers = PhoneNumberFilter::createPhoneNumbers($numberString);
 
-                                $studentGuardianRelationship->setGuardianRelationshipType($relationship);
+                        if ($phoneNumbers && strlen(trim($name)) >= 1) {
+                            foreach ($phoneNumbers as $telephoneNumber) {
+                                $telephoneNumber->setNumberOwner($guardian);
 
-                                if(strlen(trim($name)) >= 1) {
-                                    $studentGuardianRelationship->tag("class", __CLASS__);
-                                    $studentGuardianRelationship->tag("pupil_unique_number", $pupilUniqueNumber);
-                                    $studentGuardianRelationship->tag("guardian_name", $name);
+                                $telephoneNumber->tag("class", __CLASS__);
+                                $telephoneNumber->tag("number_owner", $name);
+                                $telephoneNumber->tag("telephone_number", $telephoneNumber->getNumber());
 
-                                    $this->synchronizeUsingUserTags($studentGuardianRelationship);
-                                }
-
-                                // Create telephone number for every person
-                                /** @var \Misa\Model\TelephoneNumber[] $phoneNumbers */
-                                $phoneNumbers = PhoneNumberFilter::createPhoneNumbers($numberString);
-
-                                if ($phoneNumbers && strlen(trim($name)) >= 1) {
-                                    foreach ($phoneNumbers as $telephoneNumber) {
-                                        $telephoneNumber->setNumberOwner($guardian);
-
-                                        $telephoneNumber->tag("class", __CLASS__);
-                                        $telephoneNumber->tag("number_owner", $name);
-                                        $telephoneNumber->tag("telephone_number", $telephoneNumber->getNumber());
-
-                                        $this->synchronizeUsingUserTags($telephoneNumber);
-                                    }
-                                }
+                                $this->synchronizeUsingUserTags($telephoneNumber);
                             }
                         }
                     }
                 }
-            } }
+            }
+        }
+    }
+}
 
-        include(DIR."/import-helper.php");
+include(__DIR__."/import-helper.php");
 
-        $env = [ "targetApiEndpoint"=>$targetApiEndpoint ];
 
-        $fileName = $inputFile;
+$env = [
+    "targetApiEndpoint"=>$targetApiEndpoint
+];
 
-        $importer = new OtherEmergencyContactsImporter($env); $importer->processFile($fileName, $startRow);
+$fileName = $inputFile;
+
+$importer = new OtherEmergencyContactsImporter($env);
+$importer->processFile($fileName, $startRow);
